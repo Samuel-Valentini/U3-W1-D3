b8d773b7cc3727102efbc112f8374bc9f54e55d0
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-04 16:16:56 +0100

    working selection

--- src/assets/components/SingleBook.jsx
+++ src/assets/components/SingleBook.jsx
@@ -24,15 +24,29 @@ import Card from "react-bootstrap/Card";
 // export default SingleBook;
 
 class SingleBook extends Component {
+    state = {
+        selected: false,
+    };
+
     render() {
         const { book } = this.props;
         return (
             <Col sm="6" md="4" lg="3" xxl="2" className="mb-3">
-                <Card className="my-card ">
+                <Card
+                    className={`my-card ${this.state.selected ? "bg-warning" : ""}`}
+                    // style={{ backgroundColor: sel ? "#ffe8a1" : "white" }}
+                >
                     <Card.Img
                         variant="top"
                         src={book.img}
                         className="change-ratio"
+                        onClick={() => {
+                            this.setState({
+                                selected: !this.state.selected,
+                            });
+
+                            console.log(this.state.selected);
+                        }}
                     />
                     <Card.Body className="d-flex flex-column justify-content-between">
                         <Card.Title>{book.title}</Card.Title>
